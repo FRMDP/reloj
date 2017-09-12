e0821428fd8f10d5ec9a7bb517cdfcc80364f9fd
Iampietro <ratamahatta.sindominio@yahoo.com>
2017-09-12 18:37:51 -0300

    Merge problems solved

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const horario = document.querySelector('.horario');
+const minutero = document.querySelector('.minutero');
+const segundero = document.querySelector('.segundero');
+const reloj = document.getElementById('clock');
+
+function fijarAgujas() {
+	const ahora = new Date();
+
+	const hora = ahora.getHours();
+	const gradosHora = 90 + hora * 30; // * 360 / 12
+	horario.style.transform = `rotate(${gradosHora}deg)`;
+
+	const minutos = ahora.getMinutes();
+	const gradosMinutos = 90 + minutos * 6; // *360 / 60
+	minutero.style.transform = `rotate(${gradosMinutos}deg)`;
+
+	const segundos = ahora.getSeconds();
+	const gradosSegundos = 90 + segundos * 6;
+	segundero.style.transform = `rotate(${gradosSegundos}deg)`;
+	
+	reloj.style.display = "block";
+}
+
+setInterval(fijarAgujas, 1000);
